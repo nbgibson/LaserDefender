0875929247d1dd54f9355c054aad388208653caa
WarriusBird <nbgibson@crimson.ua.edu>
2017-07-04 00:14:17 -0400

    Added player bounding
    Constrained player movement, imported package from old project.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,38 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     public float speed = 15.0f;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public float padding = 1;
+
+    private float xmin;
+    private float xmax;
+
+
+    void Start()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+        xmin = leftmost.x+ padding;
+        xmax = rightmost.x - padding;
+    }
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+            //transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            //transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
+            transform.position += Vector3.right * speed * Time.deltaTime;
         }
-	}
+        //restrict to gameplay area
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 }
+}
+
+    
