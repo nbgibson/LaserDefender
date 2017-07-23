76c477697ca5850017b11e677e5f8889f200bd86
WarriusBird <nbgibson@crimson.ua.edu>
2017-07-23 11:56:48 -0400

    Added player shot spawning

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -49,9 +49,13 @@ public class EnemySpawner : MonoBehaviour {
         float rightEdgeOfFormation = transform.position.x + (0.5f * width);
         float leftEdgeOfFormation = transform.position.x - (0.5f * width);
 
-        if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax)
+        if (leftEdgeOfFormation < xmin)
         {
-            movingRight = !movingRight;
+            movingRight = true;
+        }
+        else if(rightEdgeOfFormation > xmax)
+        {
+            movingRight = false;
         }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 15.0f;
     public float padding = 1;
+    public GameObject projectile;
+    public float projectileSpeed;
+    public float firingRate = 0.2f;
 
     private float xmin;
     private float xmax;
@@ -21,6 +24,15 @@ public class PlayerController : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            InvokeRepeating("Fire", 0.000001f, firingRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke("Fire");
+        }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //transform.position += new Vector3(-speed * Time.deltaTime, 0f, 0f);
@@ -32,10 +44,17 @@ public class PlayerController : MonoBehaviour {
             //transform.position += new Vector3(speed * Time.deltaTime, 0f, 0f);
             transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
         //restrict to gameplay area
         float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
         transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-}
+    }   
+
+    void Fire()
+    {
+        GameObject beam = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.rigidbody2D.velocity = new Vector3(0, projectileSpeed, 0);
+    }
 }
 
     
