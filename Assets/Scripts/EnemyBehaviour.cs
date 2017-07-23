b40aa927d37551d126c17951a9d2be0831281421
WarriusBird <nbgibson@crimson.ua.edu>
2017-07-23 13:03:03 -0400

    Destroy enemies
    Enemies now have health that is deducted when shot, resulting in their destruction.

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float health = 150f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Projectile missile = collision.gameObject.GetComponent<Projectile>();
+        if (missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
